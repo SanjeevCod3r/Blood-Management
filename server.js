11e613170531a61fb55686d4759abff1c8cfb565
SanjeevCod3r <Sanjeev1822.be21@chitkara.edu.in>
2023-08-08 15:24:36 +0530

    DataBase Connection Left

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const colors = require("colors");
+const morgan = require("morgan");
+const cors = require("cors");
+
+
+//<--------------------Dot Config-------------------------->
+dotenv.config();
 
 // <------------------Rest Object------------------>
 const app = express();
+
+//<-----------------Middle Wares------------------------>
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
 
 // <------------------Route------------------------->
 app.use("/api/v1/test", require("./routes/testRoutes"));
 
 // <------------------Port------------------------->
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 // <------------------Listen------------------------->
 app.listen(PORT, () => {
-    console.log("Node Server Running");
+    console.log(
+        `Node Server Running In ${process.env.DEV_MODE} ModeOn Port ${process.env.PORT}`
+    .bgBlue.white
+    );
 });
