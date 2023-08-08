cb25cc12c044aff30986361720876796c1856a3d
SanjeevCod3r <Sanjeev1822.be21@chitkara.edu.in>
2023-08-08 22:16:46 +0530

    auth done small part left

---
+++ routes/authRoutes.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const {
+  registerController,
+  loginController,
+  currentUserController,
+} = require("../controllers/authController");
+const authMiddelware = require("../middlewares/authMiddelware");
+
+const router = express.Router();
+
+//<------------------------Routes-------------------------->
+
+
+//<-------------------REGISTER || POST--------------------->
+router.post("/register", registerController);
+
+//<-------------------LOGIN || POST------------------------>
+router.post("/login", loginController);
+
+//<-----------------GET CURRENT USER || GET------------------>
+router.get("/current-user", authMiddelware, currentUserController);
+
+module.exports = router;
